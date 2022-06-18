12b6026341f8d69f73e265650a008ec03e2a7054
Pradyumna <tech.daspk@gmail.com>
2022-06-18 15:00:11 +0100

    code refactor

--- src/main/java/com/test/CollectorTest.java
+++ src/main/java/com/test/CollectorTest.java
@@ -1,5 +1,7 @@
 package com.test;
 
+import com.mycomp.model.Person;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
--- src/main/java/com/test/MyPredicate.java
+++ src/main/java/com/test/MyPredicate.java
@@ -5,29 +5,21 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
 
-public class TestPredicate {
+public class MyPredicate {
 
 	public static void main(String[] args) {
-		
 		List<String> nameList = Arrays.asList("dip","lipu","bab","bin");
-		
 		Predicate<String> nameLength = x -> x.length() >3;
 		Predicate<String> nameStartWith = Predicate.isEqual("bin");
-		
-	
-		
 		//nameList.stream().filter(nameLength.or(nameStartWith)).forEach(System.out::println);
-		
 		List<String> newList = new ArrayList<>();
-		
 		//peek is intermediary function and for each is terminal function
 		//In case of peek it will not add to the list as its not final function its any intermediary operation.
 		nameList.stream().peek(System.out::println).filter(nameLength.or(nameStartWith)).forEach(newList::add);
-		
 		System.out.println("name length:"+newList.size());
-		
-		
 
 	}
+
+
 
 }
