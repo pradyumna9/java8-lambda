f1e2a2d572273af65fb2e06a42e05ac394650c5f
Pradyumna <tech.daspk@gmail.com>
2022-11-12 11:25:09 +0000

    OptionalTestAdded

--- src/main/java/com/java8code/LearnPoint.java
+++ src/main/java/com/java8code/LearnPoint.java
@@ -1,12 +1,21 @@
 package com.java8code;
 
 import com.mycomp.model.Person;
+import org.apache.commons.collections4.CollectionUtils;
 
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 public class LearnPoint {
 
     public boolean isNameValid(String expectedName, Optional<Person> person){
         return person.map(value -> expectedName.equals(value.getName())).orElse(false);
+    }
+
+    public Optional<String> getUidMatchWithName(List<Person> personList, String matchingName){
+       return CollectionUtils.emptyIfNull(personList).stream().filter(Objects::nonNull)
+               .filter(value -> matchingName.equals(value.getName()))
+               .map(Person::getUid).findFirst();
     }
 }
--- src/test/java/com/java8code/LearnPointTest.java
+++ src/test/java/com/java8code/LearnPointTest.java
@@ -9,7 +9,10 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.Optional;
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LearnPointTest {
@@ -31,18 +34,47 @@ public class LearnPointTest {
     public void whenPersonNameIsDifferent(){
         Mockito.when(person.getName()).thenReturn("BABY");
         boolean isValidName = learnPoint.isNameValid("PRADYUMNA", Optional.of(person));
-        Assert.assertFalse(isValidName);
+        assertFalse(isValidName);
     }
     @Test
     public void whenPersonNull(){
         person = null;
         boolean isValidName = learnPoint.isNameValid("PRADYUMNA", Optional.ofNullable(person));
-        Assert.assertFalse(isValidName);
+        assertFalse(isValidName);
     }
 
     @Test
     public void whenPersonNameIsNull(){
         boolean isValidName = learnPoint.isNameValid("PRADYUMNA", Optional.ofNullable(person));
-        Assert.assertFalse(isValidName);
+        assertFalse(isValidName);
+    }
+    @Test
+    public void whenPersonListIsNull(){
+       Optional<String> result = learnPoint.getUidMatchWithName(null,"ITI");
+       assertFalse(result.isPresent());
+    }
+
+    @Test
+    public void whenSomePersonIsNull(){
+        Person person = new Person();
+        person.setName("PK");
+        person.setUid("123");
+        Person person1 = new Person();
+        person1.setName("TK");
+        person1.setUid("999");
+        Person personNull = null;
+        List<Person> personList = new ArrayList<>();
+        personList.add(person);
+        personList.add(person1);
+        personList.add(personNull);
+        Optional<String> result = learnPoint.getUidMatchWithName(personList,"TK");
+        assertEquals("999",result.get());
+    }
+
+    @Test
+    public void whenPersonListIsEmpty(){
+       List<Person> personList = Collections.emptyList();
+        Optional<String> result = learnPoint.getUidMatchWithName(personList,"TK");
+        assertFalse(result.isPresent());
     }
 }
