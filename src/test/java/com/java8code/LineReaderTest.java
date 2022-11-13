c011bd25c69063b656d6d1aeb46a99a9eef3b797
Pradyumna <tech.daspk@gmail.com>
2022-11-13 20:14:59 +0000

    JSONCompareResult Added to compare json files

--- src/test/java/com/java8code/LineReaderTest.java
+++ src/test/java/com/java8code/LineReaderTest.java
@@ -1,13 +1,21 @@
 package com.java8code;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mycomp.model.Person;
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+import org.json.JSONException;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.skyscreamer.jsonassert.JSONCompare;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.skyscreamer.jsonassert.JSONCompareResult;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -17,14 +25,21 @@ public class LineReaderTest {
 
     @InjectMocks
     private LineReader lineReader;
+    ObjectMapper mapper = new ObjectMapper();
 
     @Test
-    public void fileTest() throws IOException {
-        String fileName = "person-data.txt";
-        Path path = Paths.get("src","test","resources",fileName);
+    public void fileTest() throws IOException, JSONException {
+        Path path = getPath("person-data.txt");
+        Path jsonFile = getPath("person.json");
         List<Person> persons =  lineReader.getPersonFromFile(path);
-        Assert.assertEquals("Dipu",persons.get(0).getName());
-        Assert.assertEquals("Baby",persons.get(1).getName());
-        Assert.assertEquals("BABY",persons.get(2).getName());
+        String expectedJsonFile = new String(Files.readAllBytes(jsonFile));
+        String jsonPersonAsString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(persons);
+        JSONCompareResult jsonCompareResult = JSONCompare.compareJSON(expectedJsonFile,jsonPersonAsString, JSONCompareMode.STRICT);
+        System.out.println(jsonCompareResult.getMessage());
+        Assert.assertTrue(jsonCompareResult.passed());
+    }
+
+    private Path getPath(String fileName) {
+        return Paths.get("src","test","resources",fileName);
     }
 }
