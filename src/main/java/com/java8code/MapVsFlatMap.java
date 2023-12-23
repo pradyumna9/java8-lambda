1d7606a46d105dfdc923ef7a830fa83ce09e9dd9
Pradyumna <tech.daspk@gmail.com>
2023-12-23 12:22:42 +0000

    Flat Map Example

--- src/main/java/com/java8code/MapVsFlatMap.java
+++ src/main/java/com/java8code/MapVsFlatMap.java
@@ -1,6 +1,7 @@
 package com.java8code;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,5 +10,14 @@ public class MapVsFlatMap {
         List<String> stringList = Arrays.asList("STACK","OOVER");
         List<String[]> stringsArrayList = stringList.stream().map(s -> s.split("")).distinct().collect(Collectors.toList());
         stringsArrayList.stream().forEach(System.out::println);
+
+        List<List<String>> namesNested = Arrays.asList(
+                Arrays.asList("Jeff", "Bezos"),
+                Arrays.asList("Bill", "Gates"),
+                Arrays.asList("Mark", "Zuckerberg"));
+
+        List<String> stringList1 = namesNested.stream().flatMap(Collection::stream).collect(Collectors.toList());
+        System.out.println(stringList1);
+
     }
 }
